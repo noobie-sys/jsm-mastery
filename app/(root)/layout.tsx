1eb158f0fe11a124c99270bfd6f1024d1663c45f
Aman Gupta <amangupta954055@gmail.com>
2024-02-10 00:04:01 +0530

    working on navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Layout = ({children } :{children : React.ReactNode}) => {
   return (
-    <div>
-        Navbar
+    <div className=''>
+        <Navbar />
         {children}
-        Footer
+        <Footer />
     </div>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='fixed flex-center top-0 z-50 py-7 w-full border-b-2 border-black-200 text-white'>Navbar</nav>
+  )
+}
+
+export default Navbar
