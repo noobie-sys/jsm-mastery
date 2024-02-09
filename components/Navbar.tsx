1d1bf69b4c8901ff1bc6f972d959d2872e9e9487
Aman Gupta <amangupta954055@gmail.com>
2024-02-10 00:09:43 +0530

    adding images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='fixed flex-center top-0 z-50 py-7 w-full border-b-2 border-black-200 text-white'>Navbar</nav>
+    <nav className='fixed flex-center top-0 z-50 py-7 w-full border-b-2 border-black-200 text-white'>
+        <div className='flex-between mx-auto w-full  min-w-screen-2xl px-6 xs:px-8 sm:px-16'>
+            <Link href={'/'} >
+                <Image src={'/json-logo.svg'} alt='logo' width={55} height={40} />
+            </Link>
+        </div>
+    </nav>
   )
 }
 
